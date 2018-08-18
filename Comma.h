f26c0b3858b6825fd89629a39e144521e055a738
jjpshb <joerg@5prim.de>
2018-08-18 19:02:09 +0200

    helper class serializing lists

---
+++ Comma.h
@@ -0,0 +1,32 @@
+#ifndef _INCLUDE_COMMA_H_
+#define _INCLUDE_COMMA_H_
+
+#include <ostream>
+
+class Comma
+{
+public:
+
+  bool operator()()
+  {
+    bool temp = comma;
+    comma = true;
+
+    return temp;
+  }
+
+private:
+
+  bool comma = false;
+};
+
+std::ostream& operator << (std::ostream& out, Comma& comma)
+{
+  if (comma())
+    out << ',';
+
+  return out;
+}
+
+#endif // !_INCLUDE_COMMA_H_
+
